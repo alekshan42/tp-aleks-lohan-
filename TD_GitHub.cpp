51165061a29bcac025850004205e74d734cd9947
alekshan42 <162987119+alekshan42@users.noreply.github.com>
2024-03-18 14:47:24 +0100

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -10,6 +10,44 @@
 
 
 //Fonctions réalisées par l'étudiant 2 :
+// Fonction estPair
+int estPair(int nombre) {
+    if (nombre % 2 == 0 || nombre % 7 == 0) {
+        return 1; // Vrai
+    } else {
+        return 0; // Faux
+    }
+}
+
+// Fonction sommeNombre
+int sommeNombre(int nombre) {
+    int somme = 0;
+    while (nombre > 0) {
+        somme += nombre;
+        nombre--;
+    }
+    return somme;
+}
+
+// Fonction salaireNet
+float salaireNet(float salaireBrut) {
+    float chargeSalariale = 0.23 * salaireBrut;
+    float salaireNet = salaireBrut - chargeSalariale;
+    float prime = 0.12 * salaireNet;
+    return salaireNet + prime;
+}
+
+// Fonction plusPetit
+int plusPetit(int a, int b, int c) {
+    int min = a;
+    if (b < min) {
+        min = b;
+    }
+    if (c < min) {
+        min = c;
+    }
+    return min;
+}
 
 
 
